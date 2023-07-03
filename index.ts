fa3c09385aa0757b4469d72c016eca39a8ac21b2
guilhemheinrich <zezongo@gmail.com>
2023-07-03 17:33:41 +0200

    First commit

--- index.ts
+++ index.ts
@@ -2,13 +2,8 @@ import * as core from '@actions/core'
 import { XOasObject } from '@ts-stack/openapi-spec';
 import $RefParser from "@apidevtools/json-schema-ref-parser";
 import * as fs from 'fs-extra'
+import {languages} from './src/OperationParser'
 import OperationParser from './src/OperationParser'
-
-
-const languages = [
-    'python',
-    'R'
-] as const
     
 const main = {
     async init(config_path: string, destination: string, targeted_language: typeof languages[number]) {
@@ -18,20 +13,8 @@ const main = {
             const openapi_config: XOasObject = await $RefParser.dereference(JSON.parse(fs.readFileSync(config_path, 'utf-8'))) as XOasObject
             const operation_generator = new OperationParser(openapi_config)
             operation_generator.build()
-            console.log(fs.readdirSync(__dirname))
-            console.log(fs.pathExistsSync(`${__dirname}/src/templates/${targeted_language}/Operation.mustache`))
-            const operation_template = fs.readFileSync(`${__dirname}/src/templates/${targeted_language}/Operation.mustache`, 'utf-8')
-            
-            let extension = '.txt'
-            switch (targeted_language) {
-                case 'python':
-                    extension = '.py';
-                    break;
-                case 'R':
-                    extension = '.R';
-                    break;
-            }
-            operation_generator.digest(operation_template, destination, extension)
+            operation_generator.digest(targeted_language, destination)
+
         }
         catch (err) {
             if (err instanceof Error) core.setFailed(err.message)
